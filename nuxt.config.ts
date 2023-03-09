8005212270a5b6a6818b7b8dd407220d2c7c0b86
Chamel <chatmethar2@gmail.com>
2023-03-09 19:37:33 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  app: {
+    head: {
+      link: [{
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+      }
+]    }
+  }
+})
